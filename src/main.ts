d291dba9d2c8e25eaec6a4cddb110177bd75ba8a
Javier Rivera <riverafuentesj@yahoo.es>
2023-10-01 16:55:15 -0600

    add api deploy route

--- src/main.ts
+++ src/main.ts
@@ -3,21 +3,8 @@ import './style.css'
 import App from './App.vue'
 import { createPinia } from 'pinia';
 import { router } from "./router/router";
-import Toast, { PluginOptions, POSITION } from "vue-toastification";
+import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
-const options: PluginOptions = {
-    position: POSITION.TOP_CENTER,
-    timeout: 5000,
-    closeOnClick: true,
-    pauseOnFocusLoss: true,
-    pauseOnHover: true,
-    draggable: true,
-    draggablePercent: 0.6,
-    showCloseButtonOnHover: false,
-    hideProgressBar: true,
-    closeButton: "button",
-    icon: true,
-    rtl: false,
-  };
+
 const pinia = createPinia();
 createApp(App).use(router).use(pinia).use(Toast,{ timeout: 2000 }).mount('#app')
